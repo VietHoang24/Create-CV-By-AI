ccec093e1b6c43592d302f58d5ebb1edcc700730
Viet Salt <Vietprock@gmail.com>
2023-04-16 08:33:49 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
+import openai, { ChatCompletionRequestMessageRoleEnum } from 'openai';
+import { OpenAIApi } from 'openai';
+const completion:any = new ChatCompletionRequestMessageRoleEnum();
 
 function App() {
+  const [response, setResponse] = useState("");
+  const [input, setInput]=useState('');
+  const fetchOpenAI = async (inputText:string) => {
+    try {
+      const response = await axios.post(
+        "https://api.openai.com/v1/engines/davinci/completions",
+        {
+          prompt: inputText,
+          max_tokens: 5,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${'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'}`,
+          },
+        }
+      );
+      setResponse(response.data.choices[0].text);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleEnterText=(text:string)=>{
+    setInput(text)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={e=>handleEnterText(e.target.value)}/> 
+      <br/>
+      <button onClick={()=>fetchOpenAI(input)}>Fetch OpenAI API</button>
+    
+      <p>{response}</p>
     </div>
   );
 }
